a71c9811c21f1eb7b1af18471cc25fbaa7ae9656
Mohd Shahbaz <57652434+shahbazakon@users.noreply.github.com>
2026-06-18 23:03:48 +0530

    Lottie Added

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -74,12 +74,230 @@ class $AssetsImagesGen {
   List<AssetGenImage> get values => [detoxLogoNoBg, detoxoLogo];
 }
 
+class $AssetsLottieGen {
+  const $AssetsLottieGen();
+
+  /// File path: assets/lottie/angry.json
+  String get angry => 'assets/lottie/angry.json';
+
+  /// File path: assets/lottie/bow.json
+  String get bow => 'assets/lottie/bow.json';
+
+  /// File path: assets/lottie/crying.json
+  String get crying => 'assets/lottie/crying.json';
+
+  /// File path: assets/lottie/eating.json
+  String get eating => 'assets/lottie/eating.json';
+
+  /// File path: assets/lottie/glasses.json
+  String get glasses => 'assets/lottie/glasses.json';
+
+  /// File path: assets/lottie/hand up.json
+  String get handUp => 'assets/lottie/hand up.json';
+
+  /// File path: assets/lottie/happy.json
+  String get happy => 'assets/lottie/happy.json';
+
+  /// File path: assets/lottie/horray.json
+  String get horray => 'assets/lottie/horray.json';
+
+  /// File path: assets/lottie/laugh.json
+  String get laugh => 'assets/lottie/laugh.json';
+
+  /// File path: assets/lottie/like.json
+  String get like => 'assets/lottie/like.json';
+
+  /// File path: assets/lottie/love.json
+  String get love => 'assets/lottie/love.json';
+
+  /// File path: assets/lottie/news.json
+  String get news => 'assets/lottie/news.json';
+
+  /// File path: assets/lottie/nighty night.json
+  String get nightyNight => 'assets/lottie/nighty night.json';
+
+  /// File path: assets/lottie/nope.json
+  String get nope => 'assets/lottie/nope.json';
+
+  /// File path: assets/lottie/oh wow.json
+  String get ohWow => 'assets/lottie/oh wow.json';
+
+  /// File path: assets/lottie/please.json
+  String get please => 'assets/lottie/please.json';
+
+  /// File path: assets/lottie/pls wait.json
+  String get plsWait => 'assets/lottie/pls wait.json';
+
+  /// File path: assets/lottie/rock.json
+  String get rock => 'assets/lottie/rock.json';
+
+  /// File path: assets/lottie/sad.json
+  String get sad => 'assets/lottie/sad.json';
+
+  /// File path: assets/lottie/shocked.json
+  String get shocked => 'assets/lottie/shocked.json';
+
+  /// File path: assets/lottie/soul.json
+  String get soul => 'assets/lottie/soul.json';
+
+  /// File path: assets/lottie/thinking.json
+  String get thinking => 'assets/lottie/thinking.json';
+
+  /// File path: assets/lottie/tired.json
+  String get tired => 'assets/lottie/tired.json';
+
+  /// File path: assets/lottie/tongue 2.json
+  String get tongue2 => 'assets/lottie/tongue 2.json';
+
+  /// File path: assets/lottie/tongue.json
+  String get tongue => 'assets/lottie/tongue.json';
+
+  /// List of all assets
+  List<String> get values => [
+    angry,
+    bow,
+    crying,
+    eating,
+    glasses,
+    handUp,
+    happy,
+    horray,
+    laugh,
+    like,
+    love,
+    news,
+    nightyNight,
+    nope,
+    ohWow,
+    please,
+    plsWait,
+    rock,
+    sad,
+    shocked,
+    soul,
+    thinking,
+    tired,
+    tongue2,
+    tongue,
+  ];
+}
+
+class $AssetsTgsGen {
+  const $AssetsTgsGen();
+
+  /// File path: assets/tgs/angry.tgs
+  String get angry => 'assets/tgs/angry.tgs';
+
+  /// File path: assets/tgs/bow.tgs
+  String get bow => 'assets/tgs/bow.tgs';
+
+  /// File path: assets/tgs/crying.tgs
+  String get crying => 'assets/tgs/crying.tgs';
+
+  /// File path: assets/tgs/eating.tgs
+  String get eating => 'assets/tgs/eating.tgs';
+
+  /// File path: assets/tgs/glasses.tgs
+  String get glasses => 'assets/tgs/glasses.tgs';
+
+  /// File path: assets/tgs/hand up.tgs
+  String get handUp => 'assets/tgs/hand up.tgs';
+
+  /// File path: assets/tgs/happy.tgs
+  String get happy => 'assets/tgs/happy.tgs';
+
+  /// File path: assets/tgs/horray.tgs
+  String get horray => 'assets/tgs/horray.tgs';
+
+  /// File path: assets/tgs/laugh.tgs
+  String get laugh => 'assets/tgs/laugh.tgs';
+
+  /// File path: assets/tgs/like.tgs
+  String get like => 'assets/tgs/like.tgs';
+
+  /// File path: assets/tgs/love.tgs
+  String get love => 'assets/tgs/love.tgs';
+
+  /// File path: assets/tgs/news.tgs
+  String get news => 'assets/tgs/news.tgs';
+
+  /// File path: assets/tgs/nighty night.tgs
+  String get nightyNight => 'assets/tgs/nighty night.tgs';
+
+  /// File path: assets/tgs/nope.tgs
+  String get nope => 'assets/tgs/nope.tgs';
+
+  /// File path: assets/tgs/oh wow.tgs
+  String get ohWow => 'assets/tgs/oh wow.tgs';
+
+  /// File path: assets/tgs/please.tgs
+  String get please => 'assets/tgs/please.tgs';
+
+  /// File path: assets/tgs/pls wait.tgs
+  String get plsWait => 'assets/tgs/pls wait.tgs';
+
+  /// File path: assets/tgs/rock.tgs
+  String get rock => 'assets/tgs/rock.tgs';
+
+  /// File path: assets/tgs/sad.tgs
+  String get sad => 'assets/tgs/sad.tgs';
+
+  /// File path: assets/tgs/shocked.tgs
+  String get shocked => 'assets/tgs/shocked.tgs';
+
+  /// File path: assets/tgs/soul.tgs
+  String get soul => 'assets/tgs/soul.tgs';
+
+  /// File path: assets/tgs/thinking.tgs
+  String get thinking => 'assets/tgs/thinking.tgs';
+
+  /// File path: assets/tgs/tired.tgs
+  String get tired => 'assets/tgs/tired.tgs';
+
+  /// File path: assets/tgs/tongue.tgs
+  String get tongue => 'assets/tgs/tongue.tgs';
+
+  /// File path: assets/tgs/white flag.tgs
+  String get whiteFlag => 'assets/tgs/white flag.tgs';
+
+  /// List of all assets
+  List<String> get values => [
+    angry,
+    bow,
+    crying,
+    eating,
+    glasses,
+    handUp,
+    happy,
+    horray,
+    laugh,
+    like,
+    love,
+    news,
+    nightyNight,
+    nope,
+    ohWow,
+    please,
+    plsWait,
+    rock,
+    sad,
+    shocked,
+    soul,
+    thinking,
+    tired,
+    tongue,
+    whiteFlag,
+  ];
+}
+
 class Assets {
   const Assets._();
 
   static const $AssetsConfigGen config = $AssetsConfigGen();
   static const $AssetsContentGen content = $AssetsContentGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottieGen lottie = $AssetsLottieGen();
+  static const $AssetsTgsGen tgs = $AssetsTgsGen();
 }
 
 class AssetGenImage {
